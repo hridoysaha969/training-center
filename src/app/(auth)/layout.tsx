505c7fb9b6ecdf705123c3d75cf2764400fab205
Hridoy Saha <hridoysaha969@gmail.com>
2026-02-19 00:04:41 +0600

    Bug Fix and Optimized UI

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,17 @@
 import AdminHeader from "@/components/admin/admin-header";
 import { AppSidebar } from "@/components/admin/admin-sidebar";
-import { Separator } from "@/components/ui/separator";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import ThemeToggle from "@/components/ui/theme-toggle";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import SessionProviderWrapper from "@/contexts/SessionProviderWrapper";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-
   return (
     <SidebarProvider>
       <SessionProviderWrapper>
-        <AppSidebar user={session?.user} />
+        <AppSidebar />
 
         <div className="flex-1">
           <AdminHeader />
